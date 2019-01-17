059f3e32e97998f2d0d53d78474bb96f27a594c0
Robin <hahne.robin@gmail.com>
2019-01-17 13:21:30 +0100

    change direction

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,15 +1,50 @@
 // Rover Object Goes Here
 // ======================
-var Rover = {
-  direction:"N"
-};
+var theRover = {
+  name: "The Mars Rover",
+  year: "2019",
+  direction: "N",
+}
+
+console.log("Start direction: " + theRover.direction);
+
 // ======================
 function turnLeft(rover){
   console.log("turnLeft was called!");
+  switch(rover.direction){
+    case "N":
+      rover.direction = "W";
+      break;
+    case "E":
+      rover.direction = "N";
+      break;
+    case "S":
+      rover.direction = "E";
+      break;
+    case "W":
+      rover.direction = "S";
+      break;
+  }
+  console.log("New Direction: " + rover.direction)
 }
 
 function turnRight(rover){
   console.log("turnRight was called!");
+  switch(rover.direction){
+    case "N":
+      rover.direction = "E";
+      break;
+    case "E":
+      rover.direction = "S";
+      break;
+    case "S":
+      rover.direction = "W";
+      break;
+    case "W":
+      rover.direction = "N";
+      break;
+  }
+  console.log("New Direction: " + rover.direction)
 }
 
 function moveForward(rover){
